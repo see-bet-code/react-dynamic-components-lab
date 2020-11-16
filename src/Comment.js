89d9550c838e32c38d727969bac2d0d21269898e
see-bet-code <brittani.elan.taylor@gmail.com>
2020-11-16 09:22:40 -0500

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,22 @@
 //your code here
+import React, { Component } from 'react';
+
+export default class Comment extends Component {
+
+  state = {
+    todos: [
+      
+    ]
+  }
+
+  render() {
+    return (
+      <div className="comment" >
+        { this.props.commentText }
+      </div>
+      
+    )
+  }
+
+}
+
